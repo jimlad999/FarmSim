94438b493d01a1f520568ce919d90de01cdd0949
DESKTOP-9238O33\User <jim_lad999@hotmail.com>
2023-08-05 16:00:50 +1000

    Added regions to terrain generation
    Different regions have different chances of generating different tiles

--- FarmSim/Terrain/TerrainGenerator.cs
+++ FarmSim/Terrain/TerrainGenerator.cs
@@ -37,19 +37,53 @@ class TerrainGenerator
             tiles.Add(tileSlice);
             for (var xi = xstart; xi < xend; ++xi)
             {
-                var noiseVal = _tileGenerator.Evaluate(xi / 64.0, yi / 64.0);
-                var tileset = "grass";
-                if (noiseVal < -0.2)
-                {
-                    tileset = "water";
-                }
-                else if (noiseVal > 0.5)
-                {
-                    tileset = "rock";
-                }
+                var tileNoiseVal = _tileGenerator.Evaluate(xi / 64.0, yi / 64.0);
+                var regionNoiseVal = _regionGenerator.Evaluate(xi / 64.0, yi / 64.0);
+                var getTile = GetTilesetFunc(regionNoiseVal);
+                var tileset = getTile(tileNoiseVal);
                 tileSlice.Add(new Tile(tileset));
             }
         }
         return new Chunk(_chunkSize, tiles);
+    }
+
+    private static Func<double, string> GetTilesetFunc(double noiseVal)
+    {
+        return noiseVal switch
+        {
+            > 0.7 => GetRockyTileset,
+            < 0.2 => GetSeaTileset,
+            _ => GetPlainsTileset,
+        };
+    }
+
+    private static string GetRockyTileset(double noiseVal)
+    {
+        return noiseVal switch
+        {
+            < -0.9 => "grass",
+            < -0.5 => "sand",
+            _ => "rock",
+        };
+    }
+
+    private static string GetSeaTileset(double noiseVal)
+    {
+        return noiseVal switch
+        {
+            > 0.95 => "rock",
+            > 0.85 => "sand",
+            _ => "water",
+        };
+    }
+
+    private static string GetPlainsTileset(double noiseVal)
+    {
+        return noiseVal switch
+        {
+            > 0.8 => "rock",
+            < -0.8 => "water",
+            _ => "grass",
+        };
     }
 }
